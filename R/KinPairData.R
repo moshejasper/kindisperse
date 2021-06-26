56b0e78716b2198adeac5da6adecaf912c598eee
Moshe Jasper <moshe.jasper@unimelb.edu.au>
2021-06-26 15:00:30 +1000

    replaced KinPairData() & KinPairSimulation() functions with kin_pair_data() & kin_pair_simulation() to avoid clashes wtih S4 class names

--- R/KinPairData.R
+++ R/KinPairData.R
@@ -1,8 +1,27 @@
+#' @include DispersalModel.R
+NULL
+
 methods::setOldClass(c("tbl_df", "tbl", "data.frame"))
 
-#' Formal class "KinPairData"
+#' Formal class KinPairData
 #'
-#' @description The class \code{KinPairData} is a formal (S4) class for storing kinship and lifespan dispersal information concerning kin pairs.
+#' The class \code{KinPairData} is a formal (S4) class for storing kinship and lifespan dispersal
+#' information concerning kin pairs. It is the base class on which the \code{KinPairSimulation} class is built.
+#' The \code{KinPairData} class is used to store information about the spatial distribution of kin dyads for use
+#' in calculating axial sigmas of intergenerational dispersal as initially implemented in Jasper et al. 2019
+#' (\doi{https://doi.org/10.1111/1755-0998.13043}).
+#'
+#' This class is essentially wrapped around the \code{tbl_df} class but with (a) expectations around certain columns
+#' that must be present (\code{id1, id2, kinship}, & \code{distance} - three 'character' & one 'numeric' column), as
+#' well as (b) additional attributes (\code{kinship}, \code{lifestage}, & \code{cycle}) characterizing the close-kin
+#' dyads being stored.These attributes, as well as the embedded vector of distances, can be accessed with the methods
+#' \code{\link{kinship}}, \code{\link{lifestage}}, \code{\link{breeding_cycle}} and \code{\link{distances}}.
+#'
+#' Objects from this class are returned from the \code{\link{df_to_kinpair}} and \code{\link{csv_to_kinpair}} functions
+#' (& related), and are directly constructed with the namesake \code{KinPairData()} function.
+#' They can be passed to the \code{\link{sample_kindist}} function for filtering and subsampling, and to
+#' axial functions (including \code{\link{axials_standard}} and \code{\link{axpermute_standard}}) for estimation of
+#' axial dispersal.
 #' @slot kinship character - one of PO, FS, HS, AV, HAV, GG, 1C, H1C, GAV, HGAV, 1C1, H1C1, GGG, 2C, and H2C.
 #' @slot lifestage character - lifestage at sampling - either 'immature', 'ovipositional' or a stage
 #' corresponding to a \code{DispersalModel} custom stage
@@ -13,7 +32,7 @@ methods::setOldClass(c("tbl_df", "tbl", "data.frame"))
 #' @slot tab tbl_df. - tibble of dispersal values
 #' @return returns object of class \code{KinPairData}
 #' @export
-#'
+#' @family kdclasses
 #'
 KinPairData <- setClass("KinPairData",
   slots = list(kinship = "character", lifestage = "character", cycle = "numeric", tab = "tbl_df")
@@ -23,12 +42,13 @@ KinPairData <- setClass("KinPairData",
 ######### GENERICS and METHODS#############
 
 
-#' Access or assign kin category (generic for KinPairData class)
+#' Access or assign \code{kinship} category of \code{\link{KinPairData}} class objects
 #'
 #' @param x object with relevant method
 #'
-#' @return \code{character}. Kinship category of object
+#' @return returns \code{character} kinship category of object or \code{KinPairData} object with modified kinship category
 #' @export
+#' @family kpdmethods
 #'
 setGeneric("kinship", function(x) standardGeneric("kinship"))
 #'
@@ -36,16 +56,16 @@ setGeneric("kinship", function(x) standardGeneric("kinship"))
 #' @param x object with relevant method
 #' @param value new value to assign
 #'
-#' @return returns modified object
 #' @export
 #'
 #'
 setGeneric("kinship<-", function(x, value) standardGeneric("kinship<-"))
-#' Access or assign lifestage (generic for KinPairData class)
+#' Access or assign \code{lifestage} category of \code{\link{KinPairData}} class objects
 #'
 #' @param x object with relevant method
-#' @return \code{character} life stage of object
+#' @return returns \code{character} lifestage of object or \code{KinPairData} object with modified lifestage
 #' @export
+#' @family kpdmethods
 #'
 #'
 setGeneric("lifestage", function(x) standardGeneric("lifestage"))
@@ -54,18 +74,18 @@ setGeneric("lifestage", function(x) standardGeneric("lifestage"))
 #' @param x object with relevant method
 #' @param value new value to assign
 #'
-#' @return returns modified object with altered lifestage
 #' @export
 #'
 #'
 setGeneric("lifestage<-", function(x, value) standardGeneric("lifestage<-"))
 
-#' Access distances (generic for KinPairData class)
+#' Access or assign \code{distances} category of \code{\link{KinPairData}} class objects
 #'
 #' @param x Object of Class KinPairData
 #'
 #' @return Returns a numeric vector of kin separation distances
 #' @export
+#' @family kpdmethods
 #'
 #'
 setGeneric("distances", function(x) standardGeneric("distances"))
@@ -73,38 +93,31 @@ setGeneric("distances", function(x) standardGeneric("distances"))
 
 
 
-#'
+#' @describeIn distances
 #'
 #' @param KinPairData object of class \code{KinPairData}
 #' @param x object of class KinPairData
 #'
-#' @return numeric vector of kin separation distances
 #' @export
 #'
-#' @describeIn KinPairData access distances
 setMethod("distances", "KinPairData", function(x) x@tab$distance)
 
-#'
+#' @describeIn kinship
 #'
 #' @param KinPairData object of class \code{KinPairData}
 #'
-#' @return \code{character} kinship of \code{KinPairData} object
 #' @export
 #'
-#' @describeIn KinPairData access kin category
 setMethod("kinship", "KinPairData", function(x) x@kinship)
 
-#'
+#' @describeIn kinship
 #'
 #' @param KinPairData object of class \code{KinPairData}
 #' @param x object of class \code{KinPairData}
 #' @param value value to assign to slot
 #'
-#'@return modified object of class \code{KinPairData}
-#'
 #' @export
 #'
-#' @describeIn KinPairData assign kin category
 setMethod("kinship<-", "KinPairData", function(x, value) {
   x@kinship <- value
   validObject(x)
@@ -112,43 +125,34 @@ setMethod("kinship<-", "KinPairData", function(x, value) {
 })
 
 
+#' @describeIn lifestage
 #'
-#'
-#' @param KinPairData
-#'
-#' @return \code{character} lifestage of \code{KinPairData} object
+#' @param KinPairData object of class \code{KinPairData}
 #'
 #' @export
 #'
-#' @describeIn KinPairData access lifestage
 setMethod("lifestage", "KinPairData", function(x) x@lifestage)
 
+#' @describeIn lifestage
 #'
-#'
-#'
-#' @param KinPairData
-#'
-#' @return modified object of class \code{KinPairData}
+#' @param KinPairData object of class \code{KinPairData}
 #'
 #' @export
 #'
-#' @describeIn KinPairData assign lifestage
 setMethod("lifestage<-", "KinPairData", function(x, value) {
   x@lifestage <- value
   validObject(x)
   x
 })
 
+#' @describeIn breeding_cycle
 #'
+#' @param KinPairData object of class \code{KinPairData}
 #'
-#' @param KinPairData
-#'
-#' @return \code{numeric} number of complete breeding cycles sampled individuals have progressed through
 #' @export
-#' @describeIn KinPairData access breeding cycle
 setMethod("breeding_cycle", "KinPairData", function(x) x@cycle)
 
-#'
+#' @describeIn KinPairData standard print method
 #'
 #' @param KinPairData object of class KinPairData
 #' @param object an object of class KinpairData
@@ -157,7 +161,6 @@ setMethod("breeding_cycle", "KinPairData", function(x) x@cycle)
 #'
 #' @export
 #'
-#' @describeIn KinPairData standard print method
 setMethod(
   "show",
   "KinPairData",
@@ -175,7 +178,7 @@ setMethod(
 
 # Constructor method of KinPairData
 
-#' Constructor method for \code{KinPairData} objects.
+#' @describeIn KinPairData initialize method
 #'
 #' @param KinPairData object of class KinPairData
 #' @param .Object the KinPairData object to be constructed
@@ -192,7 +195,6 @@ setMethod(
 #'
 #' @return Returns an object of class \code{KinPairData}
 #'
-#' @describeIn KinPairData initialize method
 setMethod(
   "initialize", "KinPairData",
   function(.Object,
@@ -276,8 +278,8 @@ setMethod(
 #' @export
 #'
 #' @examples
-#' KinPairData()
-KinPairData <- function(data = NULL, kinship = NULL, lifestage = NULL, cycle = NULL) {
+#' kin_pair_data()
+kin_pair_data <- function(data = NULL, kinship = NULL, lifestage = NULL, cycle = NULL) {
   new("KinPairData", data = data, kinship = kinship, lifestage = lifestage)
 }
 
